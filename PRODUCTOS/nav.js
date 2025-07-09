614c69fabcf82b5f8cf77e82c84e1034e3df306f
61182549 <61182549@certus.edu.pe>
2025-07-09 02:23:59 -0500

    Renombro carpetas a minúsculas y actualizo rutas

--- PRODUCTOS/nav.js
+++ PRODUCTOS/nav.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navHTML = `
-    <h1><a href="/Proyecto-Crafthoria/inicio/inicio.html">Crafthoria</a></h1>
+    <h1><a href="/Proyecto-Crafthoria/INICIO/inicio.html">Crafthoria</a></h1>
     <div class="nav">
-      <a href="/Proyecto-Crafthoria/inicio/inicio.html">Inicio</a>
-      <a href="/Proyecto-Crafthoria/productos/productos.html">Productos</a>
-      <a href="/Proyecto-Crafthoria/personalizacion/personalizacion.html">Personalización</a>
-      <a href="/Proyecto-Crafthoria/blog/blog.html">Blog</a>
-      <a href="/Proyecto-Crafthoria/contactos/contacto.html">Contacto</a>
-      <a href="/Proyecto-Crafthoria/registro/registro.html">Registro</a>
+      <a href="/Proyecto-Crafthoria/INICIO/inicio.html">Inicio</a>
+      <a href="/Proyecto-Crafthoria/PRODUCTOS/productos.html">Productos</a>
+      <a href="/Proyecto-Crafthoria/PERSONALIZACION/personalizacion.html">Personalización</a>
+      <a href="/Proyecto-Crafthoria/BLOG/blog.html">Blog</a>
+      <a href="/Proyecto-Crafthoria/CONTACTOS/contacto.html">Contacto</a>
+      <a href="/Proyecto-Crafthoria/REGISTRO/registro.html">Registro</a>
     </div>
   `;
   const header = document.querySelector(".header");
   if (header) header.innerHTML = navHTML;
 });
+
