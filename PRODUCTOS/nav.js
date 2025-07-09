aab031472a11ccd222743e474d62cadd4f0068ce
61182549 <61182549@certus.edu.pe>
2025-07-09 01:48:56 -0500

    Corrijo rutas absolutas en nav.js para GitHub Pages

--- PRODUCTOS/nav.js
+++ PRODUCTOS/nav.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navHTML = `
-    <h1><a href="/Crafthoria/inicio/inicio.html">Crafthoria</a></h1>
+    <h1><a href="/Proyecto-Crafthoria/inicio/inicio.html">Crafthoria</a></h1>
     <div class="nav">
-      <a href="/Crafthoria/inicio/inicio.html">Inicio</a>
-      <a href="/Crafthoria/productos/productos.html">Productos</a>
-      <a href="/Crafthoria/personalizacion/personalizacion.html">Personalización</a>
-      <a href="/Crafthoria/blog/blog.html">Blog</a>
-      <a href="/Crafthoria/contactos/contacto.html">Contacto</a>
-      <a href="/Crafthoria/registro/registro.html">Registro</a>
+      <a href="/Proyecto-Crafthoria/inicio/inicio.html">Inicio</a>
+      <a href="/Proyecto-Crafthoria/productos/productos.html">Productos</a>
+      <a href="/Proyecto-Crafthoria/personalizacion/personalizacion.html">Personalización</a>
+      <a href="/Proyecto-Crafthoria/blog/blog.html">Blog</a>
+      <a href="/Proyecto-Crafthoria/contactos/contacto.html">Contacto</a>
+      <a href="/Proyecto-Crafthoria/registro/registro.html">Registro</a>
     </div>
   `;
   const header = document.querySelector(".header");
