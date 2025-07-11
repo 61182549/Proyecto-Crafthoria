f5986ee2fb1b271864a6402dbdaec62bb113202b
61182549 <61182549@certus.edu.pe>
2025-07-10 23:57:08 -0500

    Corrijo rutas absolutas y despliego a Firebase

--- nav.js
+++ nav.js
@@ -2,17 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector(".nav");
 
   if (nav) {
-    const base = location.hostname.includes("github.io")
-      ? "/Proyecto-Crafthoria/"
-      : "";
-
     nav.innerHTML = `
-      <a href="${base}inicio/inicio.html">Inicio</a>
-      <a href="${base}productos/productos.html">Productos</a>
-      <a href="${base}personalizacion/personalizacion.html">Personalización</a>
-      <a href="${base}blog/blog.html">Blog</a>
-      <a href="${base}contactos/contacto.html">Contactos</a>
-      <a href="${base}registro/registro.html">Registro</a>
+      <a href="/inicio/inicio.html">Inicio</a>
+      <a href="/productos/productos.html">Productos</a>
+      <a href="/personalizacion/personalizacion.html">Personalización</a>
+      <a href="/blog/blog.html">Blog</a>
+      <a href="/contactos/contacto.html">Contactos</a>
+      <a href="/registro/registro.html">Registro</a>
     `;
   }
 });
