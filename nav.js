273e6a1318f4bf1cdd5a8c5081142fe0cfac49a0
61182549 <61182549@certus.edu.pe>
2025-07-11 10:00:34 -0500

    Actualizo nav.js con rutas en mayúsculas para GitHub y Firebase

--- nav.js
+++ nav.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector(".nav");
 
-  const isGitHub = location.hostname.includes("github.io");
-  const base = isGitHub ? "/Proyecto-Crafthoria/" : "";
+  const base = location.hostname.includes("github.io")
+    ? "/Proyecto-Crafthoria/"
+    : "/";
 
   if (nav) {
     nav.innerHTML = `
-      <a href="${base}inicio/inicio.html">Inicio</a>
-      <a href="${base}productos/productos.html">Productos</a>
-      <a href="${base}personalizacion/personalizacion.html">Personalización</a>
-      <a href="${base}blog/blog.html">Blog</a>
-      <a href="${base}contactos/contacto.html">Contactos</a>
-      <a href="${base}registro/registro.html">Registro</a>
+      <a href="${base}INICIO/inicio.html">Inicio</a>
+      <a href="${base}PRODUCTOS/productos.html">Productos</a>
+      <a href="${base}PERSONALIZACION/personalizacion.html">Personalización</a>
+      <a href="${base}BLOG/blog.html">Blog</a>
+      <a href="${base}CONTACTOS/contacto.html">Contactos</a>
+      <a href="${base}REGISTRO/registro.html">Registro</a>
     `;
   }
 });
+
 
 
 
